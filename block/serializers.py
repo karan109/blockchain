ab6f30c9a03704f5bb0e7fa8fd7f100fe1873d1a
Aman Verma <amanverma1729@gmail.com>
2020-07-02 18:19:31 +0530

    Added models and serializers

---
+++ block/serializers.py
@@ -0,0 +1,24 @@
+from .models import Block, Header, Body
+from rest_framework import serializers
+
+class HeaderSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Header
+		fields = ('block_number', 'previous_hash', 'nonce', 'difficulty')
+
+class BodySerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Body
+		fields = ('content',)
+
+class ContentSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Content
+		fields = ('content',)
+	
+class BlockSerializer(serializers.ModelSerializer):
+	header = HeaderSerializer()
+	body = BodySerializer()
+	class Meta:
+		model = Block
+		fields = ('header', 'body')
